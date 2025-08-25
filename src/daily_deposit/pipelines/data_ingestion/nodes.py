b40b1205183d4ff90105e8626af4b25147462e9b
NguyenThaoVi0702 <105551975+NguyenThaoVi0702@users.noreply.github.com>
2025-08-25 14:30:29 +0700

    Create nodes.py

---
+++ src/daily_deposit/pipelines/data_ingestion/nodes.py
@@ -0,0 +1,33 @@
+import pandas as pd
+import logging
+from typing import Dict, Any
+from sqlalchemy import create_engine
+from trino.auth import BasicAuthentication
+
+log = logging.getLogger(__name__)
+
+def fetch_data_from_trino(query: str) -> pd.DataFrame:
+    """
+    Connects to Trino, executes a query, and returns the result as a DataFrame.
+    """
+    log.info(f"Connecting to Trino database to execute query...")
+    
+    try:
+        engine = create_engine(
+            "trino://10.43.128.104:18080/datalake",
+            connect_args={
+                "auth": BasicAuthentication("ai_user","Ab123456"),
+                "http_scheme": "https",
+                "verify": False
+            }
+        )
+        
+        with engine.connect() as connection:
+            df = pd.read_sql(query, connection)
+            
+        log.info(f"Successfully fetched {len(df)} rows.")
+        return df
+
+    except Exception as e:
+        log.error(f"Failed to fetch data from Trino: {e}")
+        raise
