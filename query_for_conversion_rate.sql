76cedb58412b86050e2cc9d31ad30580d6ecf604
NguyenThaoVi0702 <105551975+NguyenThaoVi0702@users.noreply.github.com>
2025-10-21 11:16:44 +0700

    Create query_for_conversion_rate.sql

---
+++ query_for_conversion_rate.sql
@@ -0,0 +1,27 @@
+SELECT 
+conversion_date,
+conversion_rate
+FROM (
+SELECT
+    *,
+    ROW_NUMBER() OVER(
+            PARTITION BY conversion_date
+            ORDER BY cob_dt DESC 
+        ) as rn
+FROM (
+    SELECT
+    DISTINCT
+        conversion_date,
+        CAST(conversion_rate AS double) AS conversion_rate,
+        cob_dt
+    FROM
+        ogl.ogl_gl_daily_rates
+    WHERE
+        conversion_date >= '2024-12-09'
+        AND conversion_type = '1009'
+        AND to_currency = 'VND'
+        AND from_currency = 'USD'
+)
+)
+WHERE 
+    rn = 1
